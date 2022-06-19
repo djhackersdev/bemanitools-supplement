d5276802a57eecc306cef0c1fe7b37a313005f8e
icex2 <djh.icex2@gmail.com>
2022-06-19 13:24:44 +0200

    Add missing sub-folders to release package

--- Module.mk
+++ Module.mk
@@ -7,6 +7,18 @@ zipdir          := $(BUILDDIR)/zip
 $(zipdir)/:
 	$(V)mkdir -p $@
 
+$(zipdir)/ddr.zip: \
+		ddr \
+		| $(zipdir)/
+	$(V)echo ... $@
+	$(V)zip -r $@ $^
+
+$(zipdir)/gfdm.zip: \
+		gfdm \
+		| $(zipdir)/
+	$(V)echo ... $@
+	$(V)zip -r $@ $^
+
 $(zipdir)/iidx.zip: \
 		iidx \
 		| $(zipdir)/
@@ -19,9 +31,18 @@ $(zipdir)/misc.zip: \
 	$(V)echo ... $@
 	$(V)zip -r $@ $^
 
+$(zipdir)/popn.zip: \
+		popn \
+		| $(zipdir)/
+	$(V)echo ... $@
+	$(V)zip -r $@ $^
+
 $(BUILDDIR)/bemanitools-supplement.zip: \
+		$(zipdir)/ddr.zip \
+		$(zipdir)/gfdm.zip \
 		$(zipdir)/iidx.zip \
 		$(zipdir)/misc.zip \
+		$(zipdir)/popn.zip \
 		README.md \
 		version \
 
